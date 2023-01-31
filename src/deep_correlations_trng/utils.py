71ad1a70dbabf2e7a2bf4c0f1e199f5d2a1b3974
Maciej Skorski <maciej.skorski@gmail.com>
2023-01-31 13:31:44 +0100

    restructure

--- src/deep_correlations_trng/utils.py
+++ src/deep_correlations_trng/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import pearsonr
 
 def read_bits(filename):
   """ Read bits from the raw file.  """
@@ -6,3 +7,8 @@ def read_bits(filename):
   bits_data = np.unpackbits(bytes_data, axis=None, count=None, bitorder='little') # unpack to bits
   bits_data = bits_data.reshape((-1,32))[:,::-1].ravel() # reverse bits order in blocks of 32
   return bits_data
+
+
+def autocorr(x,lag):
+  """ Estimate autocorrelation function at a given lag """
+  return pearsonr(x[lag:],x[:-lag]).statistic
